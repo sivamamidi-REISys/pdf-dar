fe738955df13e1075f11c3e68ba75adc3734230a
sivamamidi-REISys <siva.mamidi@reisystems.com>
2020-02-18 14:46:02 -0500

    created sample pdf

--- generatePDF.js
+++ generatePDF.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer')
- 
+
 async function printPDF() {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto('https://blog.risingstack.com', {waitUntil: 'networkidle0'});
-  const pdf = await page.pdf({ format: 'A4' });
-    
-  await browser.close();
-  return pdf
-});
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    const url = 'https://grants.hrsa.gov/2010/WebEPSExternal/Interface/Common/AccessControl/Login.aspx';
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    let d = new Date();
+    let n = d.getTime();
+    const pdf = await page.pdf({ format: 'A4', path: `temp/test_${n}.pdf` });
+
+    await browser.close();
+    return pdf
+}
+
+printPDF();
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+npm install
+node generatepdf.js
+
+References:
+https://nitayneeman.com/posts/getting-to-know-puppeteer-using-practical-examples/
